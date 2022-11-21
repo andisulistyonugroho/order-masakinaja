2feb69e58aa2ddb3ded1351ecb060765211bbed0
andisulistyonugroho <andisulistyonugroho@gmail.com>
2022-11-21 14:31:15 +0700

    get menu

--- components/orderInDate.vue
+++ components/orderInDate.vue
@@ -7,7 +7,7 @@
     fullscreen
   >
     <v-card tile>
-      <v-toolbar flat max-height="54">
+      <v-toolbar dark flat max-height="54" color="primary">
         {{ selecteddate|toDayDate }}
         <v-spacer />
         <v-btn icon small @click="$emit('closeit')">
@@ -16,12 +16,11 @@
       </v-toolbar>
       <v-divider />
       <v-card-text style="height:500px">
-        <span class="font-weight-bold">Menu</span>
-        <ul>
-          <li>Cah kangkung</li>
-          <li>Baso tumis sambel ijo</li>
-          <li>Buah</li>
-        </ul>
+        <div class="font-weight-bold">
+          Menu:
+        </div>
+        <span v-if="todaysmenu && todaysmenu.cooked_menus" class="caption" style="white-space: pre-line">{{ todaysmenu.cooked_menus.details }}</span>
+        <span v-else>belum diinput</span>
         <v-divider />
         <div v-for="santri in santries" :key="santri.id">
           <v-chip
@@ -52,12 +51,12 @@
         </div>
       </v-card-text>
       <v-divider />
-      <v-card-actions>
+      <v-card-actions v-if="childIds.length > 0">
         <v-spacer />
         <v-btn text color="primary" rounded @click="childIds=[]">
           batal
         </v-btn>
-        <v-btn color="primary" rounded depressed>
+        <v-btn color="primary" rounded depressed @click="doOrder">
           pesan
         </v-btn>
       </v-card-actions>
@@ -66,11 +65,14 @@
 </template>
 <script>
 import cloneDeep from 'lodash.clonedeep'
+import debounce from 'lodash.debounce'
+import { mapState } from 'vuex'
 export default {
   props: {
     dialog: { type: Boolean, default: false },
     selecteddate: { type: String, default: '' },
-    santris: { type: Array, default: null }
+    santris: { type: Array, default: null },
+    todaysmenu: { type: Object, default: null }
   },
   data () {
     return {
@@ -78,12 +80,12 @@ export default {
     }
   },
   computed: {
+    ...mapState({
+      userid: state => state.user.id
+    }),
     santries () {
       return cloneDeep(this.santris)
     }
-  },
-  created () {
-    console.log('order in date')
   },
   methods: {
     checkSantri (id) {
@@ -92,7 +94,32 @@ export default {
       } else {
         this.$store.dispatch('menu/removeArray', { arr: this.childIds, search: id })
       }
-    }
+    },
+    doOrder: debounce(async function () {
+      try {
+        this.$nuxt.$emit('WAIT_DIALOG', true)
+        const arrayInput = []
+        for (let i = 0; i < this.childIds.length; i++) {
+          const santri = this.santries.find(obj => obj.id === this.childIds[i])
+          arrayInput.push({
+            children_id: this.childIds[i],
+            order_date: this.selecteddate,
+            cooked_menus_id: this.cookedmenusid,
+            notes: santri.notes,
+            parent_id: this.userid
+          })
+        }
+        await this.$store.dispatch('order/addOrder', arrayInput)
+        this.$nuxt.$emit('WAIT_DIALOG', false)
+      } catch (error) {
+        this.$nuxt.$emit('WAIT_DIALOG', false)
+        this.$nuxt.$emit('EAT_SNACKBAR', {
+          view: true,
+          color: 'error',
+          message: error
+        })
+      }
+    })
   }
 }
 </script>
